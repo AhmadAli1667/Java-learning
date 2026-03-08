0bb6ff9bc1fd29af0b2b5e2af973da348f2d0168
Ahmad Ali <ahmadsh2390@gmail.com>
2026-03-09 02:59:37 +0500

    Shape project added

--- Shapetest.java
+++ Shapetest.java
@@ -53,6 +53,137 @@ abstract class ThreeDimensionalShape extends Shape {
     }
 }
 
+
+
+
+class Circle extends TwoDimensionalShape {
+    private double radius;
+    
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    
+    @Override
+    public String getDescription() {
+        return "Circle with radius " + radius;
+    }
+class Circle extends TwoDimensionalShape {
+    private double radius;
+    
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    
+    @Override
+    public String getDescription() {
+        return "Circle with radius " + radius;
+    }
+class Circle extends TwoDimensionalShape {
+    private double radius;
+    
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    
+    @Override
+    public String getDescription() {
+        return "Circle with radius " + radius;
+    }
+class Circle extends TwoDimensionalShape {
+    private double radius;
+    
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    
+    @Override
+    public String getDescription() {
+        return "Circle with radius " + radius;
+    }
+class Circle extends TwoDimensionalShape {
+    private double radius;
+    
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    
+    @Override
+    public String getDescription() {
+        return "Circle with radius " + radius;
+    }
+class Circle extends TwoDimensionalShape {
+    private double radius;
+    
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    
+    @Override
+    public String getDescription() {
+        return "Circle with radius " + radius;
+    }
+class Circle extends TwoDimensionalShape {
+    private double radius;
+    
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    
+    @Override
+    public String getDescription() {
+        return "Circle with radius " + radius;
+    }
+class Circle extends TwoDimensionalShape {
+    private double radius;
+    
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    
+    @Override
+    public String getDescription() {
+        return "Circle with radius " + radius;
+    }
 class Circle extends TwoDimensionalShape {
     private double radius;
     
